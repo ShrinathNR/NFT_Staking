bcded1251624017c2831171bf43ef429e4458896
ShrinathNR <nr.shrinath@gmail.com>
2024-08-13 22:27:03 +0530

    edit lib

--- programs/nft-staking/src/lib.rs
+++ programs/nft-staking/src/lib.rs
@@ -15,5 +15,15 @@ declare_id!("HsSHFPV1v4NPryx2DvjmMy8Z8BjWb2ucWJCmqA2N6YAs");
 pub mod nft_staking {
     use super::*;
 
-    
+    pub fn initialize_config(ctx: Context<InitializeConfig>, points_per_stake: u8, max_stake: u8, freeze_period: u32) -> Result<()> {
+        ctx.accounts.init_config(points_per_stake, max_stake, freeze_period, &ctx.bumps)
+    }
+
+    pub fn initialize_user(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.init_user(&ctx.bumps)
+    }
+
+    pub fn stake(ctx: Context<Stake>) -> Result<()> {
+        ctx.accounts.stake(&ctx.bumps)
+    }
 }
